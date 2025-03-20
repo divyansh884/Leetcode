78c5bf14a5cfa114eebe00e8ba44746a53b5ff2e
divyansh884 <154870276+divyansh884@users.noreply.github.com>
2025-03-21 01:58:41 +0530

    Time: 0 ms (100%), Space: 15.5 MB (63.03%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -1,41 +1,20 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        int count=1;
-        vector<int> odd;
-        vector<int> even;
-        ListNode* temp= head;
-        while(temp!=NULL){
-            if(count%2==0){
-                even.push_back(temp->val);
-            }
-            else
-            odd.push_back(temp->val);
-            count++;
-            temp=temp->next;
+        if (!head || !head->next) return head;
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;  
+
+        while (even && even->next) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
         }
-        ListNode* ans= new ListNode(0);
-        ListNode*temp1=ans;
-        for(int i=0;i<odd.size();i++){
-            ListNode* temp2= new ListNode(odd[i]);
-            temp1->next=temp2;
-            temp1=temp2;
-        }
-        for(int i=0;i<even.size();i++){
-            ListNode* temp2= new ListNode(even[i]);
-            temp1->next=temp2;
-            temp1=temp2;
-        }
-        return ans->next;
+
+        odd->next = evenHead;  
+        return head;
     }
 };
