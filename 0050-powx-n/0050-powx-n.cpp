d731c9f77fe94330dd7a69432460533c6112cdb5
divyansh884 <154870276+divyansh884@users.noreply.github.com>
2025-03-24 13:36:17 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.99%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
+        if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
+        if(n==0) return 1;
+        if(n<0) {
+            n = -n;
+            x = 1/x;
+        }
+        if(n%2==0){
+            return myPow(x*x, n/2);
+        }
+        else{
+            return x*myPow(x, n-1);
+        }
+    }
+};
