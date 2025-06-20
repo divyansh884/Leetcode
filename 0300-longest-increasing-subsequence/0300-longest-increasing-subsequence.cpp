82deb6c9a21e4f9e98467130b9c1cca9e7b220d2
divyansh884 <154870276+divyansh884@users.noreply.github.com>
2025-06-20 14:17:24 +0530

    Time: 0 ms (100%), Space: 14 MB (94.4%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int f(int index, vector<int> &nums, int n, int check, vector<vector<int>> &dp) {
-        if (index >= n)
-            return 0;
-        
-        if (dp[index][check + 1] != -1)
-            return dp[index][check + 1];
-        
-        int take = 0, ntake = 0;
-        
-        ntake = f(index + 1, nums, n, check, dp);
-        if (check == -1 || nums[check] < nums[index])
-            take = 1 + f(index + 1, nums, n, index, dp);
-        
-        return dp[index][check + 1] = max(take, ntake);
-    }
-
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return f(0, nums, n, -1, dp);
+       vector<int> dp;
+       dp.push_back(nums[0]);
+       for(int i=0;i<nums.size();i++){
+        if(nums[i]>dp.back()){
+            dp.push_back(nums[i]);
+        }
+        else{
+            int ind=lower_bound(dp.begin(),dp.end(),nums[i])-dp.begin();
+            dp[ind]= nums[i];
+        }
+       }
+       return dp.size();
     }
 };
