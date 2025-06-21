e836f475b8eb7d4231b9f8ba1ce898802fc582c8
divyansh884 <154870276+divyansh884@users.noreply.github.com>
2025-06-22 00:59:35 +0530

    Time: 1771 ms (11.83%), Space: 9.3 MB (73.64%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-bool poss(string &s){
-    int n=s.size();
-    for(int i=0;i<n;i++){
-        if(s[i]!=s[n-i-1])
-        return false;
+ bool check(string &s, int i, int j) {
+        while(i <= j && s[i] == s[j]) {
+            i++;
+            j--;
+        }
+        return i > j;
     }
-    return true;
-}
 
 int f(int i,string &s, int n,vector<int> &dp){
     if(i>=n)
     return 0;
-    string temp="";
     if(dp[i]!=-1) return dp[i];
     int mini=1e9;
     for(int j=i;j<n;j++){
-        temp+=s[j];
-        if(poss(temp)){
+        if(check(s,i,j)){
             int sum=1+ f(j+1,s,n,dp);
             mini=min(mini,sum);
         }
