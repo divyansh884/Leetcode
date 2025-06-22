e1cc5c0bb55cc795d1c9b14b8886cf348a32e49a
divyansh884 <154870276+divyansh884@users.noreply.github.com>
2025-06-23 02:22:36 +0530

    Time: 36 ms (16.44%), Space: 42.8 MB (13.27%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+void bfs(vector<vector<int>>& grid,vector<vector<int>>& vis,vector<pair<pair<int,int>,int>>& te,int m,int n){
+    queue<pair<pair<int,int>,int>> q;
+    for(int i=0;i<te.size();i++){
+        q.push(te[i]);
+    }
+    while(!q.empty()){
+        int row=q.front().first.first;
+        int col=q.front().first.second;
+        int step=q.front().second;
+        q.pop();
+        grid[row][col]=step;
+        if(row-1>=0){
+        if(!vis[row-1][col]) {
+            q.push({{row-1,col},step+1});
+            vis[row-1][col]=1;
+        }
+        }
+        if(col+1<m){
+        if(!vis[row][col+1]) {
+            q.push({{row,col+1},step+1});
+            vis[row][col+1]=1;
+        }
+        }
+         if(row+1<n){
+        if(!vis[row+1][col]) {
+            q.push({{row+1,col},step+1});
+            vis[row+1][col]=1;
+        }
+        }
+         if(col-1>=0){
+        if(!vis[row][col-1]) {
+            q.push({{row,col-1},step+1});
+            vis[row][col-1]=1;
+        }
+        }
+    }
+}
+    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<pair<pair<int,int>,int>> te;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==0 && vis[i][j]==0){
+                    te.push_back({{i,j},0});
+                    vis[i][j]=1;
+                }
+            }
+        }
+        bfs(grid,vis,te,m,n);
+        return grid;
+    }
+};
