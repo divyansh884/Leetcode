8292334537814b761b3d0bfdf013add2a04f776c
divyansh884 <154870276+divyansh884@users.noreply.github.com>
2025-06-27 00:23:08 +0530

    Time: 96 ms (14.38%), Space: 31.2 MB (22.98%) - LeetHub

---
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void bfs(int i, int j, vector<vector<int>>& grid,vector<vector<int>>& dis,
+             int& n) {
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>
+            pq;
+        pq.push({1, i, j});
+        int dx[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+        int dy[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
+        while (!pq.empty()) {
+            int row = pq.top()[1];
+            int col = pq.top()[2];
+            int dist = pq.top()[0];
+            pq.pop();
+            for (int dir = 0; dir < 8; dir++) {
+                int newX = row + dx[dir];
+                int newY = col + dy[dir];
+                if (newX >= 0 && newY >= 0 && newX < n && newY < n) {
+                    if(grid[newX][newY]==0 && dist+1<dis[newX][newY]){
+                        dis[newX][newY]=dist+1;
+                        pq.push({dis[newX][newY],newX,newY});
+                    }
+                }
+            }
+        }
+    }
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        if(grid[0][0]==1)
+        return -1;
+        vector<vector<int>> dis(n,vector<int>(n,1e9));
+        dis[0][0]=1;
+        bfs(0,0,grid,dis,n);
+        if(dis[n-1][n-1]==1e9)
+        return -1;
+        return dis[n-1][n-1];
+    }
+};
