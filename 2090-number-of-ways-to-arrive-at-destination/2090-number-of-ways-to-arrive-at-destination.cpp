e53cacc33a97863614e0d7c009ad2e3d9beedbf4
divyansh884 <154870276+divyansh884@users.noreply.github.com>
2025-06-27 16:09:04 +0530

    Time: 2 ms (92.01%), Space: 35.4 MB (59.08%) - LeetHub

---
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,44 @@
+const int MOD = 1e9 + 7;
+
+class Solution {
+public:
+    void bfs(int i, vector<vector<pair<int, int>>>& graph, vector<vector<long long>>& dis) {
+        priority_queue<vector<long long>, vector<vector<long long>>, greater<>> pq;
+        pq.push({0, i, 1});
+
+        while (!pq.empty()) {
+            auto top = pq.top(); pq.pop();
+            long long dist = top[0];
+            int node = top[1];
+            long long cnt = top[2];
+            if(dist>dis[node][0]) continue;
+            for (auto& [nbr, wt] : graph[node]) {
+                long long newDist = dist + wt;
+
+                if (newDist < dis[nbr][0]) {
+                    dis[nbr][0] = newDist;
+                    dis[nbr][1] = dis[node][1];
+                    pq.push({newDist, nbr, cnt});
+                } else if (newDist == dis[nbr][0]) {
+                    dis[nbr][1] = (dis[nbr][1] + dis[node][1]) % MOD;
+                }
+            }
+        }
+    }
+
+    int countPaths(int n, vector<vector<int>>& roads) {
+        vector<vector<pair<int, int>>> graph(n);
+        for (auto& road : roads) {
+            graph[road[0]].emplace_back(road[1], road[2]);
+            graph[road[1]].emplace_back(road[0], road[2]);
+        }
+
+        vector<vector<long long>> dis(n, vector<long long>(2, 0));
+        for (int i = 0; i < n; i++) dis[i][0] = 1e18;
+        dis[0][0] = 0;
+        dis[0][1] = 1;
+
+        bfs(0, graph, dis);
+        return dis[n - 1][1];
+    }
+};
