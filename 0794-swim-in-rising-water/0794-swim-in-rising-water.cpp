ba86147e01a7e46b2799ff9f39402878286baf43
divyansh884 <154870276+divyansh884@users.noreply.github.com>
2025-06-29 12:36:46 +0530

    Time: 15 ms (28.31%), Space: 14.2 MB (42.99%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -4,7 +4,7 @@ public:
              int& rown,int& coln) {
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>
             pq;
-        pq.push({0, i, j});
+        pq.push({grid[i][j], i, j});
         int dx[4] = {-1, 0, 1, 0};
         int dy[4] = {0, 1, 0, -1};
         while (!pq.empty()) {
@@ -16,8 +16,8 @@ public:
                 int newX = row + dx[dir];
                 int newY = col + dy[dir];
                 if (newX >= 0 && newY >= 0 && newX < rown && newY < coln) {
-                    int eff=max(grid[row][col],grid[newX][newY]);
-                    int maxeff=  max(eff,dist);
+                    // int eff=max(grid[row][col],grid[newX][newY]);
+                    int maxeff=  max(grid[newX][newY],dist);
                     if(maxeff<dis[newX][newY]){
                         dis[newX][newY]=maxeff;
                         pq.push({maxeff,newX,newY});
