d50aa99306ef1d81b0214b39c1bdfd5f6e739483
divyansh884 <154870276+divyansh884@users.noreply.github.com>
2025-06-30 15:44:08 +0530

    Time: 63 ms (5.99%), Space: 50 MB (5.69%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -6,14 +6,13 @@ public:
         for(int i=0;i<n;i++){
             mp[nums[i]]++;
         }
-        sort(nums.begin(),nums.end());
         int i=0,ans=0;
         while(i<n){
             if(mp[nums[i]+1]!=0){
                 int sum=mp[nums[i]]+mp[nums[i]+1];
                 ans=max(ans,sum);
             }
-            i+=mp[nums[i]];
+            i++;
         }
         return ans;
     }
