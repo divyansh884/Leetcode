c602f4ac47d52bf3b3190d78a3a5822c9fc568d5
divyansh884 <154870276+divyansh884@users.noreply.github.com>
2025-07-23 14:39:14 +0530

    Time: 72 ms (72.74%), Space: 107.9 MB (62.57%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        int sum = 0, n = nums.size(), ans = 0,prev=-1;
-        int j = 0;
+        int sum = 0, n = nums.size(), ans = 0, prev = -1;
         unordered_map<int, int> mp;
         vector<int> prefix(n);
         for (int i = 0; i < n; i++) {
@@ -11,20 +10,21 @@ public:
             else
                 prefix[i] = nums[i] + prefix[i - 1];
         }
-        while (j < n) {
-            if (mp.find(nums[j]) == mp.end()) {
-                 sum += nums[j];
-            } else {
-                if(mp[nums[j]]>=prev){
-                sum = prefix[j]-prefix[mp[nums[j]]];
-                prev=mp[nums[j]]+1;
+        for(int j=0;j<n;j++){
+            if (mp.find(nums[j]) != mp.end()) {
+                if (mp[nums[j]] >= prev) {
+                    sum = prefix[j] - prefix[mp[nums[j]]];
+                    prev = mp[nums[j]] + 1;
                 }
                 else{
-                    sum += nums[j];
+                     sum+=nums[j];
                 }
-            }ans = max(ans, sum);
+            }
+            else{
+            sum+=nums[j];
+            }
+            ans = max(ans, sum);
             mp[nums[j]] = j;
-            j++;
         }
         ans = max(ans, sum);
         return ans;
