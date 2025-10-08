e174a417097b0e94af7b797e4f3aa148d2f80bc1
divyansh884 <154870276+divyansh884@users.noreply.github.com>
2025-10-08 23:31:21 +0530

    Time: 2606 ms (5%), Space: 507.1 MB (5.34%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> ans;
-        vector<vector<int>> ans1;
-        sort(nums.begin(),nums.end());
-        int n= nums.size();
-        unordered_map<int,int> mp;
-        unordered_map<int,bool> mp1;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]=i+1;
+       set<vector<int>> ans;
+       vector<vector<int>> ans1;
+       int n=nums.size();
+       sort(nums.begin(),nums.end());
+       map<int,int> mp;
+       for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(mp.find(0-(nums[i]+nums[j]))!=mp.end())
+            ans.insert({nums[mp[0-(nums[i]+nums[j])]],nums[i],nums[j]});
         }
-        for(int i=0;i<n;i++){
-            int low = i+1, high = n-1;
-            while(low < high){
-                if(nums[i] + nums[low] + nums[high] < 0){
-                    low++;
-                }
-                else if(nums[i] + nums[low] + nums[high] > 0){
-                    high--;
-                }
-                else{
-                    ans.insert({nums[i], nums[low], nums[high]});
-                    low++;
-                    high--;
-                }
-            }
-        }
-        for(auto it: ans)
+        mp[nums[i]]=i;
+       } 
+       for(auto it: ans){
         ans1.push_back(it);
-        return ans1;
+       }
+       return ans1;
     }
 };
