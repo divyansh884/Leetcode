ca8f19a55a4a1a5d9e9f652a3e825b20cce03f8d
divyansh884 <154870276+divyansh884@users.noreply.github.com>
2025-10-23 01:23:56 +0530

    Time: 0 ms (100%), Space: 15.2 MB (32.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-
-        int low = 0; int high = n-1;
-
+        int low=0,high=nums.size()-1;
         while(low<=high){
-            int mid = high + (low-high)/2;
-
-            if(nums[mid] == target) return mid;
-
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target <= nums[mid]){
-                    high = mid - 1;
-                }
-                else low = mid + 1;
-            }
-            else{
-                if(nums[mid] <= target && target <= nums[high]){
-                    low = mid + 1;
-                }
-                else high = mid - 1;
-            }
+            int mid=low+(high-low)/2;
+            if(target>=nums[0] && nums[mid]<target && nums[mid]<nums[0])
+            high=mid-1;
+            else if(target>=nums[0] && nums[mid]<target && nums[mid]>nums[0])
+            low=mid+1;
+            else if(target>=nums[0] && nums[mid]>target)
+            high=mid-1;
+            else if(target<nums[0] && nums[mid]<target)
+            low=mid+1;
+            else if(target<nums[0] && nums[mid]>target && nums[mid]<nums[0])
+            high=mid-1;
+            else if(target<nums[0] && nums[mid]>target && nums[mid]>nums[0])
+            low=mid+1;
+            else if(nums[mid]==target) return mid;
+            else
+            low=mid+1;
         }
-
         return -1;
     }
 };
