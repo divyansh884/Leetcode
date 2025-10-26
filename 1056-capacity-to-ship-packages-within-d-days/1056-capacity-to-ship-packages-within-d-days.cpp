4b0166de5aff4bfcc2c8b587840b0e83c46ed03d
divyansh884 <154870276+divyansh884@users.noreply.github.com>
2025-10-26 18:06:44 +0530

    Time: 15 ms (32.85%), Space: 35 MB (85.55%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-int solve(vector<int>& weights, int days){
-    int day=1;
-    int temp=0;
-    for( auto it: weights){
-        
-        if(temp+it>days){
-        day++;
-        temp=0;
-        }
-        temp+=it;
-    }
-    return day;
-}
     int shipWithinDays(vector<int>& weights, int days) {
-        int left = *max_element(weights.begin(), weights.end());
-        int right = accumulate(weights.begin(), weights.end(), 0);
-        int curr=right;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            int temp= solve(weights,mid);
-            if(temp<=days){
-                curr=mid;
-                right=mid-1;
-            }
-            else
-            left=mid+1;
+        int high = 0;
+        int low = *max_element(weights.begin(),weights.end()), n = weights.size();
+        for (int i = 0; i < n; i++) {
+            high += weights[i];
         }
-        return curr;
+        int sum = high;
+        int ans = high;
+        while (low <= high) {
+            int mid = low + (high - low) / 2, need = 1, cur = 0;
+            for (int i = 0; i < weights.size(); cur += weights[i++])
+                if (cur + weights[i] > mid)
+                    cur = 0, need++;
+            if (need <= days) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
 };
