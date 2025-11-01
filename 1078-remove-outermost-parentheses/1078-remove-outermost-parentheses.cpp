f51c1215aa60fc201b28b3b6bc8de1076fe51c8e
divyansh884 <154870276+divyansh884@users.noreply.github.com>
2025-11-01 12:48:18 +0530

    Time: 0 ms (100%), Space: 9 MB (22.74%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -3,32 +3,22 @@ public:
     string removeOuterParentheses(string s) {
         stack<char> st;
         string ans="";
-        int cntf=0;
-        int cntb=0;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
-                if(cntf==0 && cntb==0)
-                cntf++;
-                else{
-                 cntf++;
-                st.push(s[i]);
-                }
+            if( s[i]=='('){
+            st.push(s[i]);
+            if(st.size()>1)
+            ans+=s[i];
             }
             else{
-                cntb++;
-                if(cntf==cntb){
-                    cntf=0;
-                    cntb=0;
+                if(st.size()==1){
+                    st.pop();
                 }
-                else
-                st.push(s[i]);
+                else{
+                    st.pop();
+                    ans+=s[i];
+                }
             }
         }
-        while(!st.empty()){
-            ans+=st.top();
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
