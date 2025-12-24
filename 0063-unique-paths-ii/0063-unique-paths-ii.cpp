c070deae7ee39defd1d8ee5b2ba31dedb67ec0a3
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2025-12-24 22:36:27 +0530

    Time: 0 ms (100%), Space: 11.8 MB (51.68%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-int f(int i,int j, vector<vector<int>>& dp,vector<vector<int>>& obstacleGrid){
-     if(i==0 && j==0)
+int f(int i,int j, int m,int n,vector<vector<int>>& obstacleGrid,vector<vector<int>> &dp){
+    if(i==m-1 && j==n-1)
     return 1;
-    if(i<0 || j<0)
+    if(i>=m || j>=n)
     return 0;
-    if(obstacleGrid[i][j]==1)
-    return 0;
-    if(dp[i][j]!=-1)return dp[i][j];
-    int l=f(i-1,j,dp,obstacleGrid);
-    int r=f(i,j-1,dp,obstacleGrid);
-    return dp[i][j]=l+r;
+    if(dp[i][j]!=-1) return dp[i][j];
+    int take=0,ntake=0;
+    if(j+1<n && obstacleGrid[i][j+1]!=1)
+    take= f(i,j+1,m,n,obstacleGrid,dp);
+    if(i+1<m && obstacleGrid[i+1][j]!=1)
+    ntake= f(i+1,j,m,n,obstacleGrid,dp);
+    return dp[i][j]=take+ntake;
 }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m=obstacleGrid.size();
-        int n=obstacleGrid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        if(obstacleGrid[m-1][n-1]==1 ||obstacleGrid[0][0]==1)
+        int m=obstacleGrid.size(),n=obstacleGrid[0].size();
+        if(obstacleGrid[0][0]==1)
         return 0;
-        return f(m-1,n-1,dp,obstacleGrid);
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return f(0,0,m,n,obstacleGrid,dp);
     }
 };
