612bbfde369dab17cc1e411e0150a856dbcff051
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2025-12-24 23:07:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-int f(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp){
-    if(i==triangle.size())
-    return 0;
+int f(int i,int j,int m,vector<vector<int>>& grid,vector<vector<int>> &dp){
+    if(i==m-1 && j<grid[i].size())
+    return grid[i][j];
     if(dp[i][j]!=-1) return dp[i][j];
-    int l= triangle[i][j]+f(i+1,j,triangle,dp);
-    int r= triangle[i][j]+f(i+1,j+1,triangle,dp);
-    return dp[i][j]=min(l,r);
-
+    int right=grid[i][j]+f(i+1,j+1,m,grid,dp);
+    int down=grid[i][j]+f(i+1,j,m,grid,dp);
+    return dp[i][j]= min(right,down);
 }
     int minimumTotal(vector<vector<int>>& triangle) {
-          int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return f(0,0,triangle,dp);
+        int m=triangle.size();
+        vector<vector<int>> dp(m,vector<int> (m,-1));
+        return f(0,0,m,triangle,dp);
     }
 };
