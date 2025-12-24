24c7534926ce11f990c7788c35ddc590677f32f8
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2025-12-24 23:11:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,6 +3,8 @@ public:
 int f(int i,int j,int m,vector<vector<int>>& grid,vector<vector<int>> &dp){
     if(i==m-1 && j<grid[i].size())
     return grid[i][j];
+    if(i==m-1 && j>=grid[i].size())
+    return 1e9;
     if(dp[i][j]!=-1) return dp[i][j];
     int right=grid[i][j]+f(i+1,j+1,m,grid,dp);
     int down=grid[i][j]+f(i+1,j,m,grid,dp);
