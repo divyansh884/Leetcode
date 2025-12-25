ded6679965a2d1bb2d941a69e4d17d8562047d4a
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2025-12-25 19:12:41 +0530

    Time: 0 ms (100%), Space: 13.4 MB (18.76%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-int f(int i,int j,int m,vector<vector<int>>& grid,vector<vector<int>> &dp){
-    if(i==m-1 && j<grid[i].size())
-    return grid[i][j];
-    if(i==m-1 && j>=grid[i].size())
-    return 1e9;
-    if(dp[i][j]!=-1) return dp[i][j];
-    int right=grid[i][j]+f(i+1,j+1,m,grid,dp);
-    int down=grid[i][j]+f(i+1,j,m,grid,dp);
-    return dp[i][j]= min(right,down);
-}
+    int f(int i, int j, int m,
+          vector<vector<int>>& grid,
+          vector<vector<int>>& dp) {
+
+        if (i == m - 1)
+            return grid[i][j];
+
+        if (dp[i][j] != INT_MAX)
+            return dp[i][j];
+
+        int down = grid[i][j] + f(i + 1, j, m, grid, dp);
+        int diag = grid[i][j] + f(i + 1, j + 1, m, grid, dp);
+
+        return dp[i][j] = min(down, diag);
+    }
+
     int minimumTotal(vector<vector<int>>& triangle) {
-        int m=triangle.size();
-        vector<vector<int>> dp(m,vector<int> (m,-1));
-        return f(0,0,m,triangle,dp);
+        int m = triangle.size();
+        vector<vector<int>> dp(m, vector<int>(m, INT_MAX));
+        return f(0, 0, m, triangle, dp);
     }
 };
