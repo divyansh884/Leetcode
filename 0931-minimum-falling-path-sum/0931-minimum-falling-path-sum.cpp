966cd11a5276e38a426e9da8d38242481e0e4aa3
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2025-12-25 19:08:34 +0530

    Time: 0 ms (100%), Space: 15.1 MB (37.99%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,20 +1,34 @@
-//Top-down Recursive with memo
 class Solution {
 public:
-    int minFallingPathSumHelper(vector<vector<int>>& matrix, int r, int c, vector<vector<int>>& dp){
-        if(r == 0 and c < matrix[0].size() and c >= 0) return matrix[r][c]; 
-        if(c >= matrix[0].size() or c < 0) return INT_MAX;
-        
-        if(dp[r][c] != INT_MAX) return dp[r][c];
-        return dp[r][c] = matrix[r][c] + min(min(minFallingPathSumHelper(matrix, r-1, c+1, dp), minFallingPathSumHelper(matrix, r-1, c, dp)), minFallingPathSumHelper(matrix, r-1, c-1, dp));
-        
-    }
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int rows = matrix.size(), cols = matrix[0].size();
-        vector<vector<int>> dp(rows+1, vector<int>(cols+1, INT_MAX));
+    int f(int i, int j, int n, vector<vector<int>>& matrix,
+          vector<vector<int>>& dp) {
+
+        if (i == n - 1)
+            return matrix[i][j];
+
+        if (dp[i][j] != INT_MAX)
+            return dp[i][j];
+
         int ans = INT_MAX;
-        for(int c=0; c < cols; c++){
-            ans = min(ans, minFallingPathSumHelper(matrix, rows-1, c, dp));
+
+        if (j - 1 >= 0)
+            ans = min(ans, matrix[i][j] + f(i + 1, j - 1, n, matrix, dp));
+
+        ans = min(ans, matrix[i][j] + f(i + 1, j, n, matrix, dp));
+
+        if (j + 1 < n)
+            ans = min(ans, matrix[i][j] + f(i + 1, j + 1, n, matrix, dp));
+
+        return dp[i][j] = ans;
+    }
+
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+        int ans = INT_MAX;
+
+        for (int j = 0; j < n; j++) {
+            ans = min(ans, f(0, j, n, matrix, dp));
         }
         return ans;
     }
