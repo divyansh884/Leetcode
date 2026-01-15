a01b968251d510b0d460c10cdf97efc494ac4bb3
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-01-15 20:14:33 +0530

    Time: 65 ms (71.44%), Space: 148.6 MB (27.88%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,25 +1,54 @@
 class MedianFinder {
-    priority_queue<int> maxheap;
-    priority_queue<int,vector<int>,greater<int>> minheap;
+    priority_queue<int> pq1;
+    priority_queue<int,vector<int>,greater<int>> pq2;
 public:
     MedianFinder() {
-        
     }
     
     void addNum(int num) {
-        maxheap.push(num);
-        minheap.push(maxheap.top());
-        maxheap.pop();
-        if(minheap.size()>maxheap.size()){
-            maxheap.push(minheap.top());
-            minheap.pop();
+        if(pq1.empty())
+        pq1.push(num);
+        else if(pq2.empty() && num>=pq1.top())
+        pq2.push((num));
+        else if(pq2.empty() && num<pq1.top()){
+            int tp=pq1.top();
+            pq1.pop();
+            pq1.push(num);
+            pq2.push(tp);
         }
-
+        else if(num>=pq2.top() && pq1.size()==pq2.size())
+        pq2.push(num);
+        else if(num>=pq2.top()&& pq1.size()!=pq2.size()){
+            if(pq1.size()>pq2.size())
+            pq2.push(num);
+            else{
+            int tp=pq2.top();
+            pq2.pop();
+            pq2.push(num);
+            pq1.push(tp);
+            }
+        }
+        else if(num>pq1.top() && num<pq2.top() && pq1.size()==pq2.size()){
+            pq1.push(num);
+        }
+        else{
+            if(pq2.size()>pq1.size())
+            pq1.push(num);
+            else{
+            int tp=pq1.top();
+            pq1.pop();
+            pq1.push(num);
+            pq2.push(tp);
+            }
+        }
     }
     
     double findMedian() {
-        if(maxheap.size()!=minheap.size()) return maxheap.top();
-        return (maxheap.top()+minheap.top())/2.0;
+        if(pq1.size()==pq2.size())
+        return (double(pq1.top())+double(pq2.top()))/2;
+        if(pq1.size()>pq2.size())
+        return (double)pq1.top();
+        return (double)pq2.top();
     }
 };
 
