74780143180473597c5298dbbdba935f07219c65
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-01-17 20:21:21 +0530

    Time: 3 ms (1.62%), Space: 36.3 MB (37.65%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -12,49 +12,48 @@
  */
 class Solution {
 public:
-    int findmax(TreeNode*& root) {
-        int ans;
-        TreeNode* temp = root;
-        ans = temp->val;
-        while (temp->right != NULL) {
-            ans = temp->val;
-            temp = temp->right;
-        }
-        ans= temp->val;
-        return ans;
-    }
-    TreeNode* deleteNode(TreeNode*& root, int key) {
-        if (root == NULL)
-            return NULL;
+    void dfs(TreeNode* root, int key, TreeNode*& ans,
+             unordered_map<TreeNode*, TreeNode*>& mp) {
+        if (!root)
+            return;
         if (root->val == key) {
-            if (root->left == NULL && root->right == NULL) {
-                delete root;
-                return NULL;
-            }
-            if (root->left != NULL && root->right == NULL) {
-                TreeNode* temp = root->left;
-                delete root;
-                return temp;
-            }
-            if (root->left == NULL && root->right != NULL) {
-                TreeNode* temp = root->right;
-                delete root;
-                return temp;
-            }
-            if (root->left != NULL && root->right != NULL) {
-                int maxi = findmax(root->left);
-                root->val = maxi;
-                root->left = deleteNode(root->left, maxi);
+            ans = root;
+        }
+        if (root->left)
+            mp[root->left] = root;
+        if (root->right)
+            mp[root->right] = root;
+        dfs(root->left, key, ans, mp);
+        dfs(root->right, key, ans, mp);
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        TreeNode* target = NULL;
+        unordered_map<TreeNode*, TreeNode*> mp;
+        dfs(root, key, target, mp);
+        if(target==NULL || !root)
+        return root;
+        TreeNode* exe=target->right;
+        if(exe==NULL){
+            if(mp[target]==NULL){
+                root=target->left;
                 return root;
             }
-        }
-        if (root->val > key) {
-            root->left = deleteNode(root->left, key);
+            if(mp[target]->right==target)
+            mp[target]->right=target->left;
+            else
+            mp[target]->left=target->left;
+            target->left=NULL;
             return root;
-        } else {
-            root->right = deleteNode(root->right, key);
-            return root;
         }
-        return NULL;
+        while(exe->left!=NULL){
+            exe=exe->left;
+        }
+        swap(exe->val,target->val);
+            if(mp[exe]->left==exe)
+            mp[exe]->left=exe->right;
+            if(mp[exe]->right==exe)
+            mp[exe]->right=exe->right;
+            exe->right=NULL;
+        return root;
     }
 };
