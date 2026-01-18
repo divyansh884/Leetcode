d4446aa60c266ce432724076051ec060fa0ec23e
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-01-18 14:25:06 +0530

    Time: 22 ms (5.8%), Space: 42.1 MB (5.89%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -6,29 +6,23 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void solve(TreeNode* root, deque<int>& mapping) {
-        if (root == NULL)
-            return;
-        solve(root->left, mapping);
-        mapping.push_back(root->val);
-        solve(root->right, mapping);
-    }
+bool dfs(TreeNode* root,int k,unordered_map<int,int> & mp){
+    if(!root)
+    return NULL;
+    if(mp.find(k-root->val)!=mp.end())
+    return true;
+    mp[root->val]++;
+    bool left=dfs(root->left,k,mp);
+    bool right=dfs(root->right,k,mp);
+    return left|| right;
+}
     bool findTarget(TreeNode* root, int k) {
-        deque<int> mapping;
-        solve(root, mapping);
-        for (auto it : mapping) {
-            int m = k - it;
-            if(m==it)
-            return false;
-            if (find(mapping.begin(),mapping.end(),m) != mapping.end())
-                return true;
-        }
-        return false;
+        unordered_map<int,int> mp;
+        return dfs(root,k,mp);
     }
 };
