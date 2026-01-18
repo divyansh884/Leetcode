c44bdb83bbfe952be90a150db202ea77bb2364a3
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-01-18 14:12:11 +0530

    Time: 15 ms (96.33%), Space: 23.5 MB (64.84%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -7,23 +7,27 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
+void f(TreeNode* root, TreeNode* p, TreeNode* q, TreeNode*& ans) {
+    if (!root)
+        return;
+    int parentvalue = root->val;
+    if ((parentvalue <= p->val && parentvalue >= q->val) ||
+        (parentvalue >= p->val && parentvalue <= q->val)) {
+        if (ans == NULL)
+            ans = root;
+    }
+    if(parentvalue>p->val && parentvalue>q->val)
+    f(root->left, p, q, ans);
+    if(parentvalue<p->val && parentvalue<q->val)
+    f(root->right, p, q, ans);
+}
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root || root == p || root == q) {
-            return root;
-        }
-        
-        TreeNode* resL = lowestCommonAncestor(root->left, p, q);
-        TreeNode* resR = lowestCommonAncestor(root->right, p, q);
-        
-        if (resL && resR) {
-            return root;
-        } else if (resL) {
-            return resL;
-        } else {
-            return resR;
-        }
+        if (!root)
+            return NULL;
+        TreeNode* ans = NULL;
+        f(root, p, q, ans);
+        return ans;
     }
 };
