a7f390aa991f0e5413b1548ff4555029c3e9eee1
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-02-14 14:23:46 +0530

    Time: 26 ms (5.08%), Space: 49 MB (9.44%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        // if (poured >= 5050)
+        //     return double(1);
+        vector<vector<double>> glass;
+        for (double i = 1; i <= 100; i++) {
+            vector<double> re(i, double(0));
+            glass.push_back(re);
+        }
+            glass[0][0]+=double(poured);
+           for( int i=0;i<100;i++){
+                for (int index = 0; index < glass[i].size(); index++) {
+                    if (glass[i][index] > double(1)) {
+                        int left = index;
+                        int right = index + 1;
+                        if (i + 1 < 100) {
+                            double dis = glass[i][index] - double(1);
+                            glass[i][index]=1;
+                            glass[i+1][left]+=dis/2;
+                            glass[i+1][right]+=dis/2;
+                        }
+                    }
+                   
+                }
+           }
+        return min(double(1),glass[query_row][query_glass]);
+    }
+};
