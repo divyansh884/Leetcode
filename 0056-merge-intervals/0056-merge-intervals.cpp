1d7ed9a9e310f133ff1fa5a7ed4dc15b2c4c4976
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-03-05 15:04:30 +0530

    Time: 4 ms (66.82%), Space: 23.9 MB (59.19%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,16 +4,12 @@ public:
         int n=intervals.size();
         vector<vector<int>> ans;
         sort(intervals.begin(),intervals.end());
-        for(int i=0;i<n;){
-            int j=i+1;
-            int st=intervals[i][0];
-            int ed=intervals[i][1];
-            while(j<n && intervals[j][0]<=ed){
-                ed= max(ed,intervals[j][1]);
-                j++;
-            }
-            ans.push_back({st,ed});
-            i=j;
+        for(int i=0;i<n;i++){
+           if(ans.empty()|| ans.back()[1]<intervals[i][0])
+           ans.push_back(intervals[i]);
+           else{
+            ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+           }
         }
         return ans;
     }
