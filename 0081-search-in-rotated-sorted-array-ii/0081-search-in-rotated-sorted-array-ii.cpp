a5588bbd2919cd48e25a3678d2be1ef9bf644467
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-03-05 23:33:41 +0530

    Time: 0 ms (100%), Space: 17.9 MB (90.16%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-        
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            
-            if (nums[mid] == target) {
-                return true;
+        int low = 0;
+        int high = nums.size() - 1, ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if(nums[mid]==target){
+                ans = mid;
+                break;
             }
-            
-            if (nums[mid] == nums[left]) {
-                left++;
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low++;
+                high--;
                 continue;
             }
-            
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+            if (nums[mid] >= nums[low]) {
+                if (nums[low] <= target && nums[mid]>target) {
+                    high=mid-1;
+                } else
+                    low=mid+1;
             } else {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+                if (nums[mid] < target && nums[high]>=target) {
+                    low=mid+1;
+                } else
+                    high=mid-1;
             }
+            
         }
-        
-        return false;        
+        if(ans!=-1)return true;
+        return false;
     }
 };
