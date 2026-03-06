19f52e22d627571e429a660a1d26cf2f2084d765
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-03-07 03:36:21 +0530

    Time: 138 ms (92.97%), Space: 132.6 MB (15%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -6,7 +6,7 @@ public:
         vector<int> primes(n,1);
         primes[0]=0;
         primes[1]=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i*i<n;i++){
             if(primes[i]==1){
                 for(int j=i*i;j<n;j+=i)
                 primes[j]=0;
