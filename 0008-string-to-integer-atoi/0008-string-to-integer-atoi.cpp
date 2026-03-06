f715fde59d434afe45b6c3576aed705e66b04022
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-03-06 16:06:04 +0530

    Time: 0 ms (100%), Space: 9.2 MB (21.46%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        long long int n = s.size();
-        long long int re = 0;
-        vector<long long int> ans;
-        bool check = false;
-        bool checkne = false;
-        long long int st = 0;
-        while (s[st] == ' ') {
-            st++;
+         long long int ans=0;
+        int i=0;
+        bool checkn=false;
+        while(i<s.size() && s[i]==' '){
+            i++;
         }
-        if (s[st] == '-' || s[st] == '+') {
-            check = true;
-            if (s[st] == '-')
-                checkne = true;
+        if(i>=s.size())
+        return ans;
+        if(s[i]=='-' || s[i]=='+'){
+            if(s[i]=='-')
+        checkn=true;
+        i++;
         }
-        for (long long int i = check ? st + 1 : st; i < n; i++) {
-            if (s[i] >= '0' && s[i] <= '9') {
-                ans.push_back(s[i] - '0');
-            } else {
-                break;
-            }
-        }
-        for (long long int j = 0; j < ans.size(); j++) {
-            if (re > INT_MAX && checkne)
-                return INT_MIN;
-            else if (re > INT_MAX && checkne == false)
+        for(int j=i;j<s.size();j++){
+            if(s[j]>='0' && s[j]<='9'){
+                ans = ans*10 + (s[j]-'0');
+                if(!checkn && ans>INT_MAX)
                 return INT_MAX;
-            re = re * 10 + (ans[j]);
+                if(checkn && ans*-1<INT_MIN)
+                return INT_MIN;
+            }
+            else
+            break;
         }
-        if (re > INT_MAX && checkne)
-            return INT_MIN;
-        else if (re > INT_MAX && checkne == false)
-            return INT_MAX;
-        if (checkne)
-            re *= -1;
-        return re;
+        if(checkn)
+        ans=ans*-1;
+        return ans;
     }
 };
