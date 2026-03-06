bc3ebb34495685d73acc54ed7a333c31dd7c1eb1
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-03-07 02:53:37 +0530

    Time: 3 ms (61.93%), Space: 10.3 MB (55.97%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
-bool issafe(vector<string> board,int row,int col, int n){
-    int dumprow=row;
-    int dumpcol=col;
-    while(row>=0 && col>=0){
-        if(board[row][col]=='Q') return false;
-        row--;
-        col--;
+    bool check(int i, int j, vector<string>& temp, int& n) {
+        int x = i, y = j;
+        while (x >= 0) {
+            if (temp[x][y] == 'Q')
+                return false;
+            x--;
+        }
+        x = i;
+        y = j;
+        while (x >= 0 && y >= 0) {
+            if (temp[x][y] == 'Q')
+                return false;
+            y--;
+            x--;
+        }
+        x = i;
+        y = j;
+        while (x >= 0 && y < n) {
+            if (temp[x][y] == 'Q')
+                return false;
+            y++;
+            x--;
+        }
+        return true;
     }
-    row=dumprow;
-    col=dumpcol;
-    while(col>=0){
-        if(board[row][col]=='Q') return false;
-        col--;
-    }
-    row=dumprow;
-    col=dumpcol;
-    while(row<n && col>=0){
-        if(board[row][col]=='Q') return false;
-        row++;
-        col--;
-    }
-    return row;
-}
-void solve(int col,vector<string> &board,vector<vector<string>> &ans,int n){
-    if(col>=n){
-        ans.push_back(board);
-        return;
-    }
-    for(int i=0;i<n;i++){
-            if(issafe(board,i,col,n)){
-                board[i][col]='Q';
-                solve(col+1,board,ans,n);
-                 board[i][col]='.';
+    void f(int i, vector<vector<string>>& ans, vector<string>& temp, int q,
+           int& n) {
+        if (i == n) {
+                ans.push_back(temp);
+            return;
+        }
+        for (int y = 0; y < n; y++) {
+            if (check(i, y, temp, n)) {
+                temp[i][y] = 'Q';
+                f(i + 1, ans, temp, q - 1, n);
+                temp[i][y] = '.';
             }
+        }
     }
-}
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
         vector<string> board(n);
         string s(n,'.');
         for(int i=0;i<n;i++){
             board[i]=s;
         }
-        solve(0,board,ans,n);
+        vector<vector<string>> ans;
+        f(0, ans, board, n, n);
         return ans;
     }
 };
