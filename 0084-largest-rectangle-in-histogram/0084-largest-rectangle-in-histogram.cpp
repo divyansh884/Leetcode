f9f1f7cf6077d0d5335fbf08e03e9df3c64743be
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-03-07 15:16:25 +0530

    Time: 23 ms (55.75%), Space: 85.8 MB (33.25%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,51 +1,45 @@
 class Solution {
-private:
-    vector<int> nextsamller(vector<int> heights, int n) {
-        stack<int> sl;
-         sl.push(-1);
+public:
+    vector<int> ns(vector<int>& arr) {
+        stack<int> st;
+        int n = arr.size();
         vector<int> ans(n);
         for (int i = n - 1; i >= 0; i--) {
-            int curr = heights[i];
-            while (sl.top() !=-1 && heights[sl.top()] >= curr) {
-                sl.pop();
-            }
-            ans[i] = sl.top();
-            sl.push(i);
+            while (!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+            if (st.empty())
+                ans[i] = n;
+            else
+                ans[i] = st.top();
+            st.push(i);
         }
         return ans;
     }
-    vector<int> prevsamller(vector<int> heights, int n) {
-        stack<int> sl;
-        sl.push(-1);
+    vector<int> ps(vector<int>& arr) {
+        stack<int> st;
+        int n = arr.size();
         vector<int> ans(n);
         for (int i = 0; i < n; i++) {
-            int curr = heights[i];
-            while (sl.top() != -1 && heights[sl.top()] >= curr) {
-                sl.pop();
-            }
-            ans[i] = sl.top();
-            sl.push(i);
+            while (!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+            if (st.empty())
+                ans[i] = -1;
+            else
+                ans[i] = st.top();
+            st.push(i);
         }
         return ans;
     }
-
-public:
     int largestRectangleArea(vector<int>& heights) {
+        vector<int> pr = ps(heights);
+        vector<int> nr = ns(heights);
         int n = heights.size();
-        vector<int> next(n);
-        next = nextsamller(heights, n);
-        vector<int> prev(n);
-        prev = prevsamller(heights, n);
         int ans = INT_MIN;
         for (int i = 0; i < n; i++) {
-            int l = heights[i];
-            cout<<next[i]<<" "<<prev[i]<<endl;
-            if (next[i] == -1) {
-                next[i] = n;
-            }
-            int b = next[i] - prev[i] - 1;
-            int newans = l * b;
-            ans = max(newans, ans);
+            int h = heights[i];
+            int b = nr[i] - pr[i] - 1;
+            int aa = h * b;
+            ans = max(ans, aa);
         }
         return ans;
     }
