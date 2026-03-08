01f0f400a2ff87f56214c4111259627e87742a06
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-03-08 15:55:18 +0530

    Time: 4 ms (27.01%), Space: 21 MB (20.17%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
- void f(int node,vector<vector<int>>& adj,vector<int> &vis,int n){
-      vis[node]=1;
-      for(int i=0;i<adj[node].size();i++){
-          if(!vis[adj[node][i]])
-          f(adj[node][i],adj,vis,n);
-      }
-  }
-    int findCircleNum(vector<vector<int>>& isConnected){
-        int n=isConnected.size();
-        vector<vector<int>> adj(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+    void f(int i, vector<vector<int>>& adj, vector<int>& visited) {
+        queue<int> q;
+        visited[i] = 1;
+        q.push(i);
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            for (auto it : adj[node]) {
+                if (!visited[it]) {
+                    q.push(it);
+                    visited[it] = 1;
                 }
             }
         }
-        vector<int> vis(n,0);
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                count++;
-                f(i,adj,vis,n);
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    continue;
+                if (isConnected[i][j] == 1)
+                    adj[i].push_back(j);
             }
         }
-        return count;
+        vector<int> visited(n, 0);
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                cnt++;
+                f(i, adj, visited);
+            }
+        }
+        return cnt;
     }
 };
