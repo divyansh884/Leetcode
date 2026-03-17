038e19403489fbec491a310d366060e128b2a99b
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-03-17 22:20:32 +0530

    Time: 18 ms (86.17%), Space: 80 MB (80.32%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -3,10 +3,10 @@ public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
         int n=matrix.size(),m=matrix[0].size();
         int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]!=0 && i>0)
-                matrix[i][j]+=matrix[i-1][j];
+        for(int i=0;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[j][i]!=0)
+                matrix[j][i]+=matrix[j-1][i];
             }
         }
         for(int i=0;i<n;i++){
