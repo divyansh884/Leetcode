15ced3a7b4a1f5a896cdac60b07dd7f625928117
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-03-20 23:56:47 +0530

    Time: 15 ms (69.63%), Space: 32 MB (68.15%) - LeetHub

--- 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
+++ 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -15,18 +15,18 @@ public:
                     }
                 }
                 sort(diff.begin(), diff.end());
-                int ins=INT_MAX;
-                for (int it = 0; it < diff.size()-1; it++) {
-                    if(diff[it+1]!=diff[it])
-                    ins= min(ins,abs(diff[it]-diff[it+1]));
+                int ins = INT_MAX;
+                for (int it = 0; it < diff.size() - 1; it++) {
+                    if (diff[it + 1] != diff[it])
+                        ins = min(ins, abs(diff[it] - diff[it + 1]));
                 }
                 diff.clear();
-                if(ins==INT_MAX)
-                ins=0;
+                if (ins == INT_MAX)
+                    ins = 0;
                 ll.push_back(ins);
             }
-            if(!ll.empty())
-            ans.push_back(ll);
+            if (!ll.empty())
+                ans.push_back(ll);
             ll.clear();
         }
         return ans;
