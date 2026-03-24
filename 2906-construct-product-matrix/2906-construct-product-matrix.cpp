e4bce3be2886e34112b65f20a53a0fb365460d39
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-03-24 11:25:05 +0530

    Time: 129 ms (7.38%), Space: 165.5 MB (11.44%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.cpp
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -1,34 +1,65 @@
 class Solution {
 public:
     int mod = 12345;
+
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
+
         vector<vector<int>> prefix(m, vector<int>(n, 1)),
             sufix(m, vector<int>(n, 1)), ans(m, vector<int>(n, -1));
-        int prev = 1;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                grid[i][j]=grid[i][j]%mod;
+                grid[i][j]=(grid[i][j]%mod);
             }
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                prefix[i][j] = prev;
-                prev = (prev * grid[i][j]) % mod;
+                if (j - 1 >= 0)
+                    prefix[i][j] = prefix[i][j - 1];
+                else {
+                    if (i - 1 >= 0)
+                        prefix[i][j] = prefix[i - 1][n - 1];
+                    else
+                        prefix[i][j] = 1;
+                }
+
+                prefix[i][j] = (prefix[i][j] * grid[i][j]) % mod;
             }
         }
-        prev = 1;
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                sufix[i][j] = prev;
-                prev = (prev * grid[i][j]) % mod;
+                if (j + 1 < n)
+                    sufix[i][j] = sufix[i][j + 1];
+                else {
+                    if (i + 1 < m)
+                        sufix[i][j] = sufix[i + 1][0];
+                    else
+                        sufix[i][j] = 1;
+                }
+
+                sufix[i][j] = (sufix[i][j] * grid[i][j]) % mod;
             }
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                ans[i][j] = (prefix[i][j] * sufix[i][j]) % mod;
+                int left, right;
+                if (j - 1 >= 0)
+                    left = prefix[i][j - 1];
+                else if (i - 1 >= 0)
+                    left = prefix[i - 1][n - 1];
+                else
+                    left = 1;
+                if (j + 1 < n)
+                    right = sufix[i][j + 1];
+                else if (i + 1 < m)
+                    right = sufix[i + 1][0];
+                else
+                    right = 1;
+
+                ans[i][j] = (left * right) % mod;
             }
         }
+
         return ans;
     }
 };
