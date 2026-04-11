39aa9a0291360723bd613c6c1576df774efac419
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-04-11 12:21:54 +0530

    Time: 527 ms (35.25%), Space: 348.3 MB (44.75%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -7,25 +7,17 @@ public:
             mp[nums[i]].push_back(i);
         }
         int ans = 1e9;
-        for (auto it : mp) {
-            if (mp[it.first].size() < 3)
-                continue;
-            int ind1 = mp[it.first][0], ind2 = mp[it.first][1],
-                ind3 = mp[it.first][2];
-            ans = min(ans,
-                      abs(ind1 - ind2) + abs(ind2 - ind3) + abs(ind3 - ind1));
-            int j = 3;
-            while (j < mp[it.first].size()) {
-                ind1 = ind2;
-                ind2 = ind3;
-                ind3 = mp[it.first][j];
-                ans = min(ans, abs(ind1 - ind2) + abs(ind2 - ind3) +
-                                   abs(ind3 - ind1));
-                j++;
+        for (auto &it : mp) {
+            auto &v = it.second;
+            if (v.size() < 3) continue;
+            for (int i = 0; i + 2 < v.size(); i++) {
+                int i1 = v[i];
+                int i2 = v[i + 1];
+                int i3 = v[i + 2];
+                ans = min(ans, abs(i1 - i2) + abs(i2 - i3) + abs(i3 - i1));
             }
         }
-        if (ans == 1e9)
-            return -1;
-        return ans;
+
+        return (ans == 1e9) ? -1 : ans;
     }
 };
