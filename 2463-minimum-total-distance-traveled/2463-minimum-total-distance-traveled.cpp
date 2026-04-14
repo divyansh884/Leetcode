18a893a22800ad6caee51fa01ea63e6b0fbfa38e
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-04-14 13:08:31 +0530

    Time: 35 ms (58.22%), Space: 11.5 MB (98.63%) - LeetHub

---
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long dp[101][101];
+
+    long long solve(int i, int j, vector<int>& robot, vector<vector<int>>& factory) {
+        if (i >= robot.size()) return 0;
+        if (j >= factory.size()) return 1e18;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        long long res = solve(i, j + 1, robot, factory); // skip factory
+
+        long long cost = 0;
+        for (int k = 0; k < factory[j][1] && i + k < robot.size(); k++) {
+            cost += abs(robot[i + k] - factory[j][0]);
+            res = min(res, cost + solve(i + k + 1, j + 1, robot, factory));
+        }
+
+        return dp[i][j] = res;
+    }
+
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        sort(robot.begin(), robot.end());
+        sort(factory.begin(), factory.end());
+        memset(dp, -1, sizeof(dp));
+        return solve(0, 0, robot, factory);
+    }
+};
