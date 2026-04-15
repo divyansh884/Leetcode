d714e72fb5f9b8852950010c41fc76ea9dc89884
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-04-15 12:26:26 +0530

    Time: 0 ms (100%), Space: 18.2 MB (68.33%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -1,39 +1,16 @@
 class Solution {
 public:
     int closestTarget(vector<string>& words, string target, int startIndex) {
-        int cnt1 = 0, cnt2 = 0, n = words.size();
-        int i = startIndex;
-        bool check1 = false;
-        bool check2 = false;
-        while (true) {
+        int n = words.size();
+        int ans = INT_MAX;
+
+        for (int i = 0; i < n; i++) {
             if (words[i] == target) {
-                check1 = true;
-                break;
-            } else {
-                i = (i + 1) % n;
-                cnt1++;
+                int dist = abs(i - startIndex);
+                ans = min(ans, min(dist, n - dist));
             }
-            if (i == startIndex)
-                break;
         }
-        i = startIndex;
-        while (true) {
-            if (words[i] == target) {
-                check2 = true;
-                break;
-            } else {
-                i = (i - 1 + n) % n;
-                cnt2++;
-            }
-            if (i == startIndex)
-                break;
-        }
-        if (check1 && check2)
-            return min(cnt1, cnt2);
-        if (check1)
-            return cnt1;
-        if (check2)
-            return cnt2;
-        return -1;
+
+        return ans == INT_MAX ? -1 : ans;
     }
 };
