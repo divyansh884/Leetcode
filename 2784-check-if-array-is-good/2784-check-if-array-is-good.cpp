13a11c3a76ba5feb7fb8e8c72454d0830aa2b9df
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-05-14 12:34:52 +0530

    Time: 5 ms (6.98%), Space: 27.6 MB (11.11%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int n = nums.size();
+        if(n<=1)
+        return false;
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]]++;
+        }
+        for (int i = 1; i <= n - 1; i++) {
+            if (i != n - 1 && mp[i] != 1)
+                return false;
+            else if (i == n - 1 && mp[i] != 2)
+                return false;
+        }
+        return true;
+    }
+};
