ed3c252f2300ba9af7923bb92647116a8cf7456c
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-05-31 16:29:59 +0530

    Time: 32 ms (17.37%), Space: 34.4 MB (96.26%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,66 +1,36 @@
-#define LC_HaCK
-#ifdef LC_HaCK
-const auto __ = []() {
-    struct _ {
-        static void __() { std::ofstream("display_runtime.txt") << 0 << std::endl; }
-    };
-    std::atexit(&_::__);
-    return 0;
-}();
-#endif
-
 class Solution {
 public:
-void bfs(vector<vector<int>>& grid,vector<vector<int>>& vis,vector<pair<pair<int,int>,int>>& te,int m,int n){
-    queue<pair<pair<int,int>,int>> q;
-    for(int i=0;i<te.size();i++){
-        q.push(te[i]);
-    }
-    while(!q.empty()){
-        int row=q.front().first.first;
-        int col=q.front().first.second;
-        int step=q.front().second;
-        q.pop();
-        grid[row][col]=step;
-        if(row-1>=0){
-        if(!vis[row-1][col]) {
-            q.push({{row-1,col},step+1});
-            vis[row-1][col]=1;
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size(), m = mat[0].size();
+        queue<pair<int,int>> q;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(mat[i][j] == 0) q.push({i, j});
+                else mat[i][j] = 1e9;
+            }
         }
-        }
-        if(col+1<m){
-        if(!vis[row][col+1]) {
-            q.push({{row,col+1},step+1});
-            vis[row][col+1]=1;
-        }
-        }
-         if(row+1<n){
-        if(!vis[row+1][col]) {
-            q.push({{row+1,col},step+1});
-            vis[row+1][col]=1;
-        }
-        }
-         if(col-1>=0){
-        if(!vis[row][col-1]) {
-            q.push({{row,col-1},step+1});
-            vis[row][col-1]=1;
-        }
-        }
-    }
-}
-    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
-        int n=grid.size(),m=grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        vector<pair<pair<int,int>,int>> te;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==0 && vis[i][j]==0){
-                    te.push_back({{i,j},0});
-                    vis[i][j]=1;
+
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, 1, 0, -1};
+
+        while(!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+
+            for(int k = 0; k < 4; k++) {
+                int nr = r + dx[k];
+                int nc = c + dy[k];
+
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m &&
+                   mat[nr][nc] > mat[r][c] + 1) {
+                    
+                    mat[nr][nc] = mat[r][c] + 1;
+                    q.push({nr, nc});
                 }
             }
         }
-        bfs(grid,vis,te,m,n);
-        return grid;
+
+        return mat;
     }
 };
