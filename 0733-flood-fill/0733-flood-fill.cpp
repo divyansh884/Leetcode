2171eb7ffbfee2dafa35637fc9879b6c9825baa9
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-05-31 14:20:43 +0530

    Time: 5 ms (2.18%), Space: 19 MB (8.98%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
-void bfs(int i, int j, vector<vector<int>>& grid,vector<vector<int>>& vis,int &m,int &n,int &color,int &check){
-    queue<vector<int>> q;
-    q.push({i,j});
-    vis[i][j]=1;
-    while(!q.empty()){
-        vector<int> temp=q.front();
-        q.pop();
-        grid[temp[0]][temp[1]]=color;
-        if(temp[0]-1>=0){
-        if(grid[temp[0]-1][temp[1]]==check  && !vis[temp[0]-1][temp[1]]) {
-            q.push({temp[0]-1,temp[1]});
-            vis[temp[0]-1][temp[1]]=1;
+    vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc,
+                                  int color) {
+        int m = grid.size();
+        int n = grid[0].size();
+        queue<vector<int>> q;
+        vector<int> dx = {-1, 0, 1, 0};
+        vector<int> dy = {0, -1, 0, 1};
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        q.push({sr,sc});
+        int org=grid[sr][sc];
+        grid[sr][sc]=color;
+        while (!q.empty()) {
+            int row = q.front()[0];
+            int col = q.front()[1];
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int nr = row + dx[i];
+                int nc = col + dy[i];
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && !vis[nr][nc] &&
+                    grid[nr][nc] ==org ) {
+                    vis[nr][nc] = 1;
+                    grid[nr][nc] = color;
+                    q.push({nr, nc});
+                }
+            }
         }
-        }
-        if(temp[1]+1<m){
-        if(grid[temp[0]][temp[1]+1]==check  && !vis[temp[0]][temp[1]+1]) {
-            q.push({temp[0],temp[1]+1});
-            vis[temp[0]][temp[1]+1]=1;
-        }
-        }
-         if(temp[0]+1<n){
-        if(grid[temp[0]+1][temp[1]]==check  && !vis[temp[0]+1][temp[1]]) {
-            q.push({temp[0]+1,temp[1]});
-            vis[temp[0]+1][temp[1]]=1;
-        }
-        }
-         if(temp[1]-1>=0){
-        if(grid[temp[0]][temp[1]-1]==check  && !vis[temp[0]][temp[1]-1]) {
-            q.push({temp[0],temp[1]-1});
-            vis[temp[0]][temp[1]-1]=1;
-        }
-        }
-    }
-}
-    vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int color) {
-        int n=grid.size(),m=grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        int check=grid[sr][sc];
-        bfs(sr,sc,grid,vis,m,n,color,check);
         return grid;
     }
 };
