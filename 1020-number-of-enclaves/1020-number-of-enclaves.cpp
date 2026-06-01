b30d23a5133ed00597c9caeef884252540f29b9e
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-06-01 14:01:43 +0530

    Time: 16 ms (17.53%), Space: 33.7 MB (51.96%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    void f(int i, int j, vector<vector<int>>& b, vector<vector<int>>& vis,
+           int& m, int& n) {
+        queue<pair<int, int>> q;
+        q.push({i, j});
+        vector<int> dx = {-1, 0, 1, 0};
+        vector<int> dy = {0, -1, 0, 1};
+        while (!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dx[i];
+                int nc = c + dy[i];
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && b[nr][nc] == 1 &&
+                    !vis[nr][nc]) {
+                    vis[nr][nc] = 1;
+                    q.push({nr, nc});
+                }
+            }
+        }
+    }
+    int numEnclaves(vector<vector<int>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            if (!vis[0][i] && board[0][i] == 1) {
+                vis[0][i] = 1;
+                f(0, i, board, vis, m, n);
+            }
+            if (!vis[m - 1][i] && board[m - 1][i] == 1) {
+                vis[m - 1][i] = 1;
+                f(m - 1, i, board, vis, m, n);
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if (!vis[i][0] && board[i][0] == 1) {
+                vis[i][0] = 1;
+                f(i, 0, board, vis, m, n);
+            }
+            if (!vis[i][n - 1] && board[i][n - 1] == 1) {
+                vis[i][n - 1] = 1;
+                f(i, n - 1, board, vis, m, n);
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!vis[i][j] && board[i][j] == 1)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
