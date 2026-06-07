6643c6f9808adaf48addac59d412d28d583d7cd1
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-06-07 15:40:59 +0530

    Time: 11 ms (98.86%), Space: 221.3 MB (99.72%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -1,49 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* createBinaryTree(vector<vector<int>>& des) {
-        unordered_map<int, vector<int>> mp;
-        map<int, int> nodes;
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
 
-        for (auto &d : des) {
-            mp[d[0]].push_back(d[1]);
-            mp[d[0]].push_back(d[2]);
-            nodes[d[1]]++;
+        static const auto fast_io = [](){
+            std::ios::sync_with_stdio(false);
+            std::cin.tie(nullptr);
+            return nullptr;
+        }();
+
+        static TreeNode nodes[100001] = {};
+        static int is_child[100001] = { false };
+        static int generation[100001] = { 0 };
+        static int generation_id = 0;
+
+        ++generation_id;
+
+        for(const auto& v : descriptions){
+            const int parent = v[0], child = v[1];
+            const bool left_child = v[2];
+            if(generation[parent] != generation_id){
+                TreeNode* temp = &nodes[parent];
+                temp->left = temp->right = nullptr;
+                temp->val = parent;
+                generation[parent] = generation_id;
+            }
+            if(generation[child] != generation_id){
+                TreeNode* temp = &nodes[child];
+                temp->left = temp->right = nullptr;
+                temp->val = child;
+                generation[child] = generation_id;
+            }
+            TreeNode* parent_node = &nodes[parent];
+            TreeNode* child_node = &nodes[child];
+            if(left_child){
+                parent_node->left = child_node;
+            } else {
+                parent_node->right = child_node;
+            }
+            is_child[child] = true;
         }
-
-        unordered_map<int, TreeNode*> tree;
-
-        TreeNode* ans = nullptr;
-        queue<TreeNode*> q;
-
-        for (auto it : mp) {
-            if (nodes.find(it.first) == nodes.end()) {
-                ans = new TreeNode(it.first);
-                tree[it.first] = ans;
-                q.push(ans);
+        int root = -1;
+        for(const auto& v : descriptions){
+            const int parent = v[0];
+            if(!is_child[parent]){
+                root = parent;
                 break;
             }
         }
-
-        while (!q.empty()) {
-            TreeNode* node = q.front();
-            q.pop();
-
-            for (int i = 0; i < mp[node->val].size(); i += 2) {
-                int num = mp[node->val][i];
-                int isleft = mp[node->val][i + 1];
-
-                if (!tree.count(num))
-                    tree[num] = new TreeNode(num);
-
-                if (isleft)
-                    node->left = tree[num];
-                else
-                    node->right = tree[num];
-
-                q.push(tree[num]);
-            }
+        for(const auto& v : descriptions){
+            const int parent = v[0], child = v[1];
+            is_child[parent] = false;
+            is_child[child] = false;
         }
 
-        return ans;
+        return &nodes[root];
     }
 };
