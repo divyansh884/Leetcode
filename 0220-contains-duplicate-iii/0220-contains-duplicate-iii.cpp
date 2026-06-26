0a8a6a150b5623e81d76f9f89dd7fc78900ed3a7
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-06-26 18:40:46 +0530

    Time: 337 ms (5.04%), Space: 182.4 MB (5.04%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
@@ -2,29 +2,43 @@ class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int id, int vd) {
         int n = nums.size();
-        map<int, int> mp;
-        for (int i = 1; i <= min(n - 1, id); i++) {
-            mp[nums[i]]++;
-        }
-        int st = 0;
+        if (id == 0) return false;
+
+        map<long long, int> mp;
+
         int ed = min(n - 1, id);
+
+        // Initial window: [1, ed]
+        for (int i = 1; i <= ed; i++)
+            mp[nums[i]]++;
+
+        int st = 0;
+
         while (st < n) {
-            int r = nums[st] + vd;
-            int l = nums[st] - vd;
+            long long l = 1LL * nums[st] - vd;
+            long long r = 1LL * nums[st] + vd;
+
             auto it = mp.lower_bound(l);
-            if (it != mp.end() && it->first <= r) {
+            if (it != mp.end() && it->first <= r)
                 return true;
-            }
-            ed++;
-            if (ed < n)
+
+            // Add next element to the window
+            if (ed + 1 < n) {
+                ed++;
                 mp[nums[ed]]++;
-            st++;
-            if (st < n)
-                mp[nums[st]]--;
-            if (st<n && mp[nums[st]] == 0) {
-                mp.erase(nums[st]);
             }
+
+            // Remove the leftmost element of the current window
+            int rem = st + 1;
+            if (rem < n) {
+                mp[nums[rem]]--;
+                if (mp[nums[rem]] == 0)
+                    mp.erase(nums[rem]);
+            }
+
+            st++;
         }
+
         return false;
     }
 };
