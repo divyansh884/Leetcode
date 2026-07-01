2d3c1152365526bb6e2e1d0c2dd29b0d2191503a
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-07-01 11:55:23 +0530

    Time: 554 ms (31.91%), Space: 178.7 MB (47.39%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int maximumSafenessFactor(vector<vector<int>>& grid) {
         int n = grid.size();
-
         vector<vector<int>> dis(n, vector<int>(n, 1e9));
         queue<pair<int, int>> q;
         for (int i = 0; i < n; i++) {
@@ -13,14 +12,11 @@ public:
                 }
             }
         }
-
         int dx[] = {-1, 0, 1, 0};
         int dy[] = {0, -1, 0, 1};
-
         while (!q.empty()) {
             auto [r, c] = q.front();
             q.pop();
-
             for (int k = 0; k < 4; k++) {
                 int nr = r + dx[k];
                 int nc = c + dy[k];
@@ -32,34 +28,25 @@ public:
                 }
             }
         }
-
         priority_queue<vector<int>> pq;
         vector<vector<int>> dist(n, vector<int>(n, -1));
-
         pq.push({dis[0][0], 0, 0});
         dist[0][0] = dis[0][0];
-
         while (!pq.empty()) {
             auto cur = pq.top();
             pq.pop();
-
             int safe = cur[0];
             int r = cur[1];
             int c = cur[2];
-
             if (r == n - 1 && c == n - 1)
                 return safe;
-
             if (safe < dist[r][c])
                 continue;
-
             for (int k = 0; k < 4; k++) {
                 int nr = r + dx[k];
                 int nc = c + dy[k];
-
                 if (nr >= 0 && nr < n && nc >= 0 && nc < n) {
                     int newsafe = min(safe, dis[nr][nc]);
-
                     if (newsafe > dist[nr][nc]) {
                         dist[nr][nc] = newsafe;
                         pq.push({newsafe, nr, nc});
@@ -67,7 +54,6 @@ public:
                 }
             }
         }
-
         return 0;
     }
 };
