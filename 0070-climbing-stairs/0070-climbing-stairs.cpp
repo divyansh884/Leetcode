14690bdd8e9383095d5932c3128c7e089d93f67b
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-07-14 00:37:12 +0530

    Time: 0 ms (100%), Space: 8.6 MB (19.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-int f(int strt,int n,vector<int> &dp){
-if(strt>=n){
-    if(strt==n)
-    return 1;
-    return 0;
-}
-if(dp[strt]!=-1) return dp[strt];
-int op1=f(strt+1,n,dp);
-int op2=f(strt+2,n,dp);
-return dp[strt]=op1+op2;
-}
     int climbStairs(int n) {
-       int strt=0;
-       vector<int> dp(n+1,-1);
-       return f(strt,n,dp); 
+        vector<int> vec(n+1,-1);
+        vec[0]=1;
+        for(int i=1;i<=n;i++){
+            vec[i]=vec[i-1];
+            if(i-2>=0)
+            vec[i]+=vec[i-2];
+        }
+        return vec[n];
     }
 };
