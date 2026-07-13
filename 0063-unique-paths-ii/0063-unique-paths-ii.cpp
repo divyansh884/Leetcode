48353755283a203882f88c0bb433fc0fe612a9f8
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-07-13 23:48:46 +0530

    Time: 0 ms (100%), Space: 11.8 MB (72.47%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-int f(int i,int j, int m,int n,vector<vector<int>>& obstacleGrid,vector<vector<int>> &dp){
-    if(i==m-1 && j==n-1)
-    return 1;
-    if(i>=m || j>=n)
-    return 0;
-    if(dp[i][j]!=-1) return dp[i][j];
-    int take=0,ntake=0;
-    if(j+1<n && obstacleGrid[i][j+1]!=1)
-    take= f(i,j+1,m,n,obstacleGrid,dp);
-    if(i+1<m && obstacleGrid[i+1][j]!=1)
-    ntake= f(i+1,j,m,n,obstacleGrid,dp);
-    return dp[i][j]=take+ntake;
-}
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m=obstacleGrid.size(),n=obstacleGrid[0].size();
-        if(obstacleGrid[0][0]==1)
+    int uniquePathsWithObstacles(vector<vector<int>>& og) {
+        int m = og.size(), n = og[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        bool check = true;
+        for (int i = 0; i < n; i++) {
+            if (og[0][i] == 0 && check)
+                dp[0][i] = 1;
+            else {
+                dp[0][i] = 0;
+                check = false;
+            }
+        }
+        check = true;
+        for (int i = 0; i < m; i++) {
+            if (og[i][0] == 0 && check)
+                dp[i][0] = 1;
+            else {
+                dp[i][0] = 0;
+                check = false;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (og[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        if(dp[m - 1][n - 1]!=-1)
+        return dp[m-1][n-1];
         return 0;
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return f(0,0,m,n,obstacleGrid,dp);
     }
 };
