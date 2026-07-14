457f64755af1ce302cbe981ff62d41f7ceda1e76
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-07-14 14:17:59 +0530

    Time: 8 ms (58.97%), Space: 45 MB (12.18%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,15 +3,19 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         if(s.size()==0)
         return 0;
-        sort(s.begin(),s.end(),greater<int>());
-        sort(g.begin(),g.end(),greater<int>());
+        sort(s.begin(),s.end());
+        sort(g.begin(),g.end());
         int index=0;
+        int i=0;
         int cnt=0;
-        for(int i=0;i<g.size();i++){
+        while(i<g.size()){
             if(g[i]<=s[index]){
                 index++;
+                i++;
                 cnt++;
             }
+            else if(g[i]>s[index])
+            index++;
             if(index>=s.size())
             break;
         }
