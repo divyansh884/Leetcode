31f70fd602b9c4c776f350538ef7411b8fa0b3e0
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-07-14 16:05:16 +0530

    Time: 36 ms (50.77%), Space: 48.7 MB (44.34%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-int f(int i,int left,vector<int> &coins, vector<vector<int>> &dp){
-    if(left==0)
-    return 0;
-    if(i>=coins.size())
-    return 1e9;
-    if(dp[i][left]!=-1) return dp[i][left];
-    int take=1e9;
-    if(left-coins[i]>=0)
-    take=1+f(i,left-coins[i],coins,dp);
-    int ntake=f(i+1,left,coins,dp);
-    return dp[i][left]=min(take,ntake);
-}
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
-        int re=f(0,amount,coins,dp);
-        if(re==1e9)
-        return -1;
-        return re;
+        int n = coins.size();
+        const int INF = 1e9;
+
+        vector<vector<int>> dp(n, vector<int>(amount + 1, INF));
+
+        dp[0][0] = 0;
+        for (int j = coins[0]; j <= amount; j++) {
+            if (j % coins[0] == 0)
+                dp[0][j] = j / coins[0];
+        }
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = 0;
+            for (int j = 1; j <= amount; j++) {
+                int ntake = dp[i - 1][j];
+                int take = INF;
+                if (coins[i] <= j)
+                    take = 1 + dp[i][j - coins[i]];
+
+                dp[i][j] = min(take, ntake);
+            }
+        }
+
+        return dp[n - 1][amount] == INF ? -1 : dp[n - 1][amount];
     }
 };
