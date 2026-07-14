8c2b466971816e0217854286dcffadc1acc55366
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-07-15 00:28:07 +0530

    Time: 47 ms (12.49%), Space: 27.5 MB (30.9%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-int f(int i,int j,string &text1,string &text2,vector<vector<int>> &dp){
-    if(i<0 || j<0)
-    return 0;
-    if(dp[i][j]!=-1) return dp[i][j];
-    int match=0,nmatch1=0,nmatch2=0;
-    if(text1[i]==text2[j])
-    match=1+ f(i-1,j-1,text1,text2,dp);
-    else{
-    nmatch1=f(i-1,j,text1,text2,dp);
-    nmatch2=f(i,j-1,text1,text2,dp);
+    int f(int i, int j, string& s1, string& s2, vector<vector<int>>& dp) {
+        int n = s1.size();
+        int m = s2.size();
+        if (i == n || j == m)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int take1 = 0, ntake1 = 0, ntake2 = 0;
+        if (s1[i] == s2[j]) {
+            take1 = 1 + f(i + 1, j + 1, s1, s2, dp);
+        } else {
+            ntake1 = f(i + 1, j, s1, s2, dp);
+            ntake2 = f(i, j + 1, s1, s2, dp);
+        }
+        return dp[i][j] = max(take1, max(ntake1, ntake2));
     }
-    return dp[i][j]=max(match,max(nmatch1,nmatch2));
-}
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),-1));
-        return f(text1.size()-1,text2.size()-1,text1,text2,dp);
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return f(0, 0,text1, text2,dp);
     }
 };
