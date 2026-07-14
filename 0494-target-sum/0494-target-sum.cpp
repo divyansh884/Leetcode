46df4a14fc68a77d66d09834ef1cd5a14b6265b0
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-07-14 16:47:04 +0530

    Time: 19 ms (44.07%), Space: 41.4 MB (15.1%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,30 +1,29 @@
 class Solution {
-  public:
-  int f(int index,int target,vector<vector<int>> &dp,vector<int> &arr,int n){
-      if(index==n){
-          if(target==0)
-          return 1;
-          return 0;
-      }
-      if(target<0)
-      return 0;
-      int take=0,ntake=0;
-      if(dp[index][target]!= -1) return dp[index][target];
-      take+= f(index+1,target-arr[index],dp,arr,n);
-      ntake+= f(index+1,target,dp,arr,n);
-      return dp[index][target]=take+ntake;
-  }
-    int findTargetSumWays(vector<int>& arr, int d) {
-        int index=0;
-        int target=0;
-        for(int i=0;i<arr.size();i++){
-            target+=arr[i];
-        }
-        if(target<d || (target-d)%2!=0)
-        return 0;
-        target-=d;
-        target/=2;
-        vector<vector<int>> dp(arr.size()+1,vector<int>(target+1,-1));
-        return f(0,target,dp,arr,arr.size());
+public:
+    int n;
+    const int OFFSET = 1000;
+
+    int f(int i, int sum, vector<int>& nums, int target,
+          vector<vector<int>>& dp) {
+
+        if (i == n)
+            return sum == target;
+
+        if (dp[i][sum + OFFSET] != -1)
+            return dp[i][sum + OFFSET];
+
+        int add = f(i + 1, sum + nums[i], nums, target, dp);
+        int sub = f(i + 1, sum - nums[i], nums, target, dp);
+
+        return dp[i][sum + OFFSET] = add + sub;
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+
+        n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>(2001, -1));
+
+        return f(0, 0, nums, target, dp);
     }
 };
