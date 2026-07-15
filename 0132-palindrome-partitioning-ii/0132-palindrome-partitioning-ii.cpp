81f8946906facd2fc1597eaf0afa47bad899815e
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-07-15 19:33:21 +0530

    Time: 1753 ms (18.04%), Space: 9.4 MB (74.8%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,40 +1,30 @@
-#define LC_HaCK
-#ifdef LC_HaCK
-const auto __ = []() {
-    struct _ {
-        static void __() { std::ofstream("display_runtime.txt") << 0 << std::endl; }
-    };
-    std::atexit(&_::__);
-    return 0;
-}();
-#endif
-
 class Solution {
 public:
- bool check(string &s, int i, int j) {
-        while(i <= j && s[i] == s[j]) {
+    bool check(string& s, int i, int j) {
+        while (i <= j && s[i] == s[j]) {
             i++;
             j--;
         }
         return i > j;
     }
 
-int f(int i,string &s, int n,vector<int> &dp){
-    if(i>=n)
-    return 0;
-    if(dp[i]!=-1) return dp[i];
-    int mini=1e9;
-    for(int j=i;j<n;j++){
-        if(check(s,i,j)){
-            int sum=1+ f(j+1,s,n,dp);
-            mini=min(mini,sum);
+    int f(int i, string& s, int n, vector<int>& dp) {
+        if (i >= n)
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+        int mini = 1e9;
+        for (int j = i; j < n; j++) {
+            if (check(s, i, j)) {
+                int sum = 1 + f(j + 1, s, n, dp);
+                mini = min(mini, sum);
+            }
         }
+        return dp[i] = mini;
     }
-   return dp[i]=mini;
-}
     int minCut(string s) {
-        int n= s.size();
-        vector<int> dp(n+1,-1);
-        return f(0,s,n,dp)-1;
+        int n = s.size();
+        vector<int> dp(n + 1, -1);
+        return f(0, s, n, dp) - 1;
     }
 };
