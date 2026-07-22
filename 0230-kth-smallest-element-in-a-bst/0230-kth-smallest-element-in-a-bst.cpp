2ffc0f65bda2ecb5ab5c12137da3b39de30fb59a
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-07-22 15:37:52 +0530

    Time: 0 ms (100%), Space: 24.3 MB (66.71%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -6,22 +6,27 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-void travel(TreeNode* root,vector<int>& ans){
-    if(root==NULL)
-    return;
-    travel(root->left,ans);
-    ans.push_back(root->val);
-    travel(root->right,ans);
-}
+    void dfs(TreeNode* root, int &cnt, int& ans) {
+        if (root == NULL) {
+            return;
+        }
+        dfs(root->left, cnt, ans);
+        cnt--;
+        if (cnt == 0) {
+            ans = root->val;
+            return;
+        }
+        dfs(root->right, cnt, ans);
+    }
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> ans;
-        travel(root,ans);
-        sort(ans.begin(),ans.end());
-        return ans[k-1];
+        int ans = -1;
+        dfs(root, k, ans);
+        return ans;
     }
 };
