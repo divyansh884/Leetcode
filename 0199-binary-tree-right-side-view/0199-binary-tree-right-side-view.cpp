5d08c0ce5bc735ced0d60053bf3c547247d658df
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-07-24 02:23:43 +0530

    Time: 0 ms (100%), Space: 15.5 MB (7.48%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -12,6 +12,26 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        
+        vector<int> ans;
+        if(root==NULL)
+        return ans;
+        queue<pair<TreeNode*,int>> q;
+      q.push({root,0});
+      map<int,int> mp;
+      while(!q.empty()){
+          auto it= q.front();
+          q.pop();
+          TreeNode* n=it.first;
+          int pos=it.second;
+          mp[pos]=n->val;
+          if(n->left)
+          q.push({n->left,pos+1});
+          if(n->right)
+          q.push({n->right,pos+1});
+      }
+      for(auto it:mp){
+          ans.push_back(it.second);
+      }
+      return ans;
     }
 };
