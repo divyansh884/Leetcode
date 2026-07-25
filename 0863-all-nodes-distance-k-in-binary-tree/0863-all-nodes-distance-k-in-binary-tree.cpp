12f57e890a86ef63fcd8a41c780864b54d73406b
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-07-25 15:27:34 +0530

    Time: 4 ms (65.3%), Space: 15.2 MB (84.31%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,49 +9,45 @@
  */
 class Solution {
 public:
-void f(TreeNode* root,unordered_map<TreeNode*,TreeNode*> &parent){
-    if(root==NULL)
-    return;
-    if(root->left)
-    parent[root->left]=root;
-    if(root->right)
-    parent[root->right]=root;
-    f(root->left,parent);
-    f(root->right,parent);
-}
+    void f(TreeNode* root, unordered_map<TreeNode*, TreeNode*>& parent) {
+        if (root == NULL)
+            return;
+        if (root->left)
+            parent[root->left] = root;
+        if (root->right)
+            parent[root->right] = root;
+        f(root->left, parent);
+        f(root->right, parent);
+    }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<TreeNode*,TreeNode*> parent;
-        f(root,parent);
+        unordered_map<TreeNode*, TreeNode*> parent;
+        f(root, parent);
         unordered_map<TreeNode*, bool> visited;
-        queue<TreeNode*> q;
-        q.push(target);
+        queue<pair<TreeNode*,int>> q;
+        q.push({target,0});
         visited[target] = true;
-        int dist=0;
+        vector<int> ans;
         while(!q.empty()){
-            int size = q.size();
-            if (dist++ == k) break;
-            for(int i=0;i<size;i++){
-                TreeNode* temp=q.front();
-                q.pop();
-                if(temp->left && !visited[temp->left]){
-                    q.push(temp->left);
-                    visited[temp->left]=true;
-                }
-                 if(temp->right && !visited[temp->right]){
-                    q.push(temp->right);
-                    visited[temp->right]=true;
-                }
-                if(parent[temp] && !visited[parent[temp]]){
-                    q.push(parent[temp] );
-                    visited[parent[temp]]=true;
-                }
+            int dist=q.front().second;
+            TreeNode* node=q.front().first;
+            q.pop();
+            if(dist==k){
+                ans.push_back(node->val);
+                continue;
+            }
+            if(node->left && !visited[node->left]){
+                visited[node->left]=1;
+                q.push({node->left,dist+1});
+            }
+            if(node->right && !visited[node->right]){
+                visited[node->right]=1;
+                q.push({node->right,dist+1});
+            }
+            if(parent[node] && !visited[parent[node]]){
+                visited[parent[node]]=1;
+                q.push({parent[node],dist+1});
             }
         }
-        vector<int> res;
-        while (!q.empty()) {
-            res.push_back(q.front()->val);
-            q.pop();
-        }
-        return res;
+        return ans;
     }
 };
