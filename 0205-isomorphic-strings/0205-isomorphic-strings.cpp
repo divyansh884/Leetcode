b247c754e0713078c792e981f3fae2c508fa2302
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-08-03 20:35:16 +0530

    Time: 9 ms (6.26%), Space: 9.3 MB (83.26%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char> mp;
-        unordered_map<char,bool> mp1;
-        unordered_map<char,bool> mp2;
-        for(int i=0;i<s.size();i++){
-            if(mp1[s[i]]==true && mp[s[i]]!=t[i]){
+        map<char, char> mp1, mp2;
+        int n=s.size();
+        for (int i = 0; i < n; i++) {
+            if (mp1.find(s[i]) != mp1.end() && mp1[s[i]] != t[i])
                 return false;
-            }
-            else if(mp1[s[i]]!=true){
-                mp1[s[i]]=true;
-                if(mp2[t[i]]==true)
-                return false;
-                mp2[t[i]]=true;
-                mp[s[i]]=t[i];
-            }
+            else if (mp2.find(t[i])!=mp2.end() && mp2[t[i]]!=s[i])
+            return false;
+            mp1[s[i]]=t[i];
+            mp2[t[i]]=s[i];
         }
-        return  true;
+        return true;
     }
 };
