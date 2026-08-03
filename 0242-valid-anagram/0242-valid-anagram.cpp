f123b5b8f1af93db38937cc82b5437bd4b3b7dc2
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-08-03 20:41:26 +0530

    Time: 8 ms (6.98%), Space: 9.7 MB (76.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,10 +5,7 @@ public:
         return false;
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=t[i])
-            return false;
-        }
-        return true;
+        
+        return s==t;
     }
 };
