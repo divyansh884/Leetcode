22137092650109ab0de0ac8e8cc9b49abecbf481
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-08-03 20:04:42 +0530

    Time: 3 ms (34.12%), Space: 10.5 MB (51.66%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,32 +3,24 @@ public:
     string reverseWords(string s) {
         stack<string> st;
         string temp = "";
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] >= 'a' && s[i] <= 'z')
-                temp += s[i];
-            else if (s[i] >= 'A' && s[i] <= 'Z')
-                temp += s[i];
-            else if (s[i] >= '0' && s[i] <= '9')
-            temp+=s[i];
-            else{
-                if(temp!="")
-                st.push(temp);
-                temp="";
+        int n=s.size();
+        for (int i = 0; i < n; i++) {
+            if (s[i] != ' ')
+                temp.push_back( s[i]);
+            else {
+                if (temp != "")
+                    st.push(temp);
+                temp = "";
             }
         }
-        if(temp!="")
-        st.push(temp);
-        string ans="";
-        while(!st.empty()){
-            if(st.size()==1){
-                ans+=st.top();
-                st.pop();
-            }
-            else{
-                ans+=st.top();
-                st.pop();
-                ans+=" ";
-            }
+        if (temp != "")
+            st.push(temp);
+        string ans = "";
+        while (!st.empty()){
+            ans+=st.top();
+            st.pop();
+            if(!st.empty())
+            ans.push_back(' ');
         }
         return ans;
     }
