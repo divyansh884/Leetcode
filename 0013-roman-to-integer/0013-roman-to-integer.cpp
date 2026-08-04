cf48edf8244aeaf26122ee9ad43375cc49080470
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-08-04 12:27:28 +0530

    Time: 10 ms (31.12%), Space: 12.4 MB (63.03%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int romanToInt(string s) {
         unordered_map<char, int> m;
-        
+
         m['I'] = 1;
         m['V'] = 5;
         m['X'] = 10;
@@ -10,16 +10,14 @@ public:
         m['C'] = 100;
         m['D'] = 500;
         m['M'] = 1000;
-        
         int ans = 0;
-        
-        for(int i = 0; i < s.length(); i++){
-            if(m[s[i]] < m[s[i+1]]){
-                ans -= m[s[i]];
-            }
-            else{
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (i == s.size() - 1)
                 ans += m[s[i]];
-            }
+            else if (m[s[i + 1]] > m[s[i]])
+                ans -= m[s[i]];
+            else
+            ans+=m[s[i]];
         }
         return ans;
     }
