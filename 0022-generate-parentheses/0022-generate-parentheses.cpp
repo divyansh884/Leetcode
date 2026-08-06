5ecaf188c4424b258f24a0c2f205bd6b666cc9cf
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-08-06 20:07:15 +0530

    Time: 28 ms (5.09%), Space: 44.7 MB (5%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
+bool check(string &s){
+    stack<char> st;
+    for(auto it: s){
+        if(st.empty())
+        st.push(it);
+        else if(st.top()=='(' && it==')')
+        st.pop();
+        else
+        st.push(it);
+    }
+    return st.empty();
+}
     void f(vector<string>& ans, string& temp, int& n1,int& n2) {
         if (n1 == 0 && n2 == 0) {
+            if(check(temp))
             ans.push_back(temp);
             return;
         }
@@ -12,7 +25,7 @@ public:
             temp.pop_back();
             n1++;
         }
-        if (n2 >n1) {
+        if (n2 !=0) {
                 temp.push_back(')');
                 n2--;
                 f(ans, temp, n1, n2);
