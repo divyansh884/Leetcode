995b86741c631d0a0eb978adb84f2982b17b435d
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-08-09 20:54:27 +0530

    Time: 3 ms (31.38%), Space: 11.4 MB (17.51%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-void f(set<vector<int>> &ans,vector<int> &temp,vector<int> &nums,int i){
-    if(i==nums.size()){
-        ans.insert(temp);
-        return;
+    void f(set<vector<int>>& ans, vector<int>& temp, vector<int>& nums, int i) {
+        if (i == nums.size()) {
+            ans.insert(temp);
+            return;
+        }
+        temp.push_back(nums[i]);
+        f(ans, temp, nums, i + 1);
+        temp.pop_back();
+        f(ans, temp, nums, i + 1);
     }
-    temp.push_back(nums[i]);
-    f(ans,temp,nums,i+1);
-    temp.pop_back();
-    f(ans,temp,nums,i+1);
- }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
         set<vector<int>> a;
-        vector<int> temp;
-        int i=0;
-        f(a,temp,nums,i);
+        for (int i = 0; i < pow(2, nums.size()); i++) {
+            vector<int> temp;
+            for(int j=0;j<32;j++){
+                if((i>>j)&1)
+                temp.push_back(nums[j]);
+            }
+            a.insert(temp);
+        }
         vector<vector<int>> ans(a.begin(),a.end());
         return ans;
     }
