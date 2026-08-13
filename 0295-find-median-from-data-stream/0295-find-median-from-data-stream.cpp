a87faf5050b6867b3692300200f942b272e82634
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-08-13 20:53:49 +0530

    Time: 252 ms (5.01%), Space: 148.4 MB (93.14%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,55 +1,65 @@
 class MedianFinder {
     priority_queue<int> pq1;
-    priority_queue<int, vector<int>, greater<int>> pq2;
-
+    priority_queue<int,vector<int>,greater<int>> pq2;
 public:
-    MedianFinder() {}
-
+    MedianFinder() {
+        
+    }
+    
     void addNum(int num) {
-        if (pq1.empty())
-            pq1.push(num);
-        else if (pq2.empty() && num >= pq1.top())
-        pq2.push(num);
-        else if(pq2.empty() && num<pq1.top()){
-                int tt = pq1.top();
+        if(pq1.empty())
+        pq1.push(num);
+        else if(pq2.empty()){
+            if(num>pq1.top())
+            pq2.push(num);
+            else{
+                int temp=pq1.top();
                 pq1.pop();
-                pq2.push(tt);
+                pq2.push(temp);
                 pq1.push(num);
+            }
         }
-        else if(num>=pq2.top() && pq2.size()==pq1.size()){
+        else if(num<=pq1.top()){
+            if(pq1.size()>pq2.size()){
+                int temp=pq1.top();
+                pq1.pop();
+                pq2.push(temp);
+                pq1.push(num);
+            }
+            else{
+                pq1.push(num);
+            }
+        }
+        else if(num>=pq2.top()){
+             if(pq2.size()>pq1.size()){
+                int temp=pq2.top();
+                pq2.pop();
+                pq1.push(temp);
                 pq2.push(num);
-        }
-        else if(num>=pq2.top() && pq2.size()!=pq1.size()){
-                if (pq1.size() > pq2.size())
-                    pq2.push(num);
-                else {
-                    int tt = pq2.top();
-                    pq2.pop();
-                    pq1.push(tt);
-                    pq2.push(num);
-                }
-        }
-        else if(num>pq1.top() && num<pq2.top() && pq1.size()==pq2.size()){
-                pq1.push(num);
+            }
+            else{
+                pq2.push(num);
+            }
         }
         else{
-                if (pq2.size() > pq1.size())
-                    pq1.push(num);
-                else {
-                    int tt = pq1.top();
-                    pq1.pop();
-                    pq2.push(tt);
-                    pq1.push(num);
-                }
+            if(pq2.size()>=pq1.size())
+            pq1.push(num);
+            else
+            pq2.push(num);
         }
     }
-
+    
     double findMedian() {
-        if (pq1.size() == pq2.size())
-            return (double(pq1.top()) + double(pq2.top())) / 2;
-        if (pq1.size() > pq2.size())
-            return (double)pq1.top();
-        return (double)pq2.top();
+        cout<<pq1.size()<<pq2.size()<<endl;
+        if(pq1.size()==pq2.size()){
+            double temp1=pq1.top();
+            double temp2=pq2.top();
+            double tt=(temp1+temp2)/2;
+        return tt;
+        }
+        else if(pq2.size()>pq1.size())
+        return double(pq2.top());
+        return double(pq1.top());
     }
 };
 
