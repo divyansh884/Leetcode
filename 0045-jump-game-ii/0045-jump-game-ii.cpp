8d78315aa5a6b5a5deb2a9dbf0e38e1e2169b50d
Divyansh Sharma <154870276+divyansh884@users.noreply.github.com>
2026-08-15 10:55:49 +0530

    Time: 0 ms (100%), Space: 20.6 MB (54.53%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int currentIndex, vector<int>& memo) {
-        if (currentIndex >= nums.size() - 1) return 0;
-        if (memo[currentIndex] != -1) return memo[currentIndex];
-        
-        int minJumps = nums.size() + 1;
-        
-        for (int step = nums[currentIndex]; step >= 1; --step) {
-            int jumps = 1 + solve(nums, currentIndex + step, memo);
-            minJumps = min(minJumps, jumps);
-        }
-        
-        return memo[currentIndex] = minJumps;
-    }
-
     int jump(vector<int>& nums) {
-        vector<int> memo(nums.size(), -1);
-        return solve(nums, 0, memo);
+        int maxind = 0;
+        int prevmaxind = 0;
+        int step = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            if (prevmaxind >= n - 1)
+                break;
+
+            if (i + nums[i] > maxind) {
+                maxind = i + nums[i];
+            }
+
+            if (i == prevmaxind) {
+                step++;
+                prevmaxind = maxind;
+            }
+        }
+
+        return step;
     }
 };
